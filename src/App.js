5a999874799a5d264082e617706af25c6ba84653
Derick Olson <olsonderick@gmail.com>
2023-02-27 08:33:47 -0500

    test api call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [titles, setTitles] = useState([]);
+  useEffect(() => {
+    fetch(`https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=${process.env.NYT_API_KEY}`)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json.results)
+        setTitles(json.results);
+      })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Titles</div>
+        {titles.map(title =>
+          <a href={title.amazon_product_url}>
+            {title.book_details[0].title} by {title.book_details[0].author}
+          </a>
+        )}
       </header>
     </div>
   );
